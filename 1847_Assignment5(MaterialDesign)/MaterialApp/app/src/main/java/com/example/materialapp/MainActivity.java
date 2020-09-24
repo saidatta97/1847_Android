abbb5e0b57e5bdee59946b5001b8a87313e4e6d1
saidatta97 <saidattasawant.ss@gmail.com>
2020-09-24 17:54:32 +0530

    Assignment 5 REGISTER Page using Material Design

---
+++ 1847_Assignment5(MaterialDesign)/MaterialApp/app/src/main/java/com/example/materialapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.materialapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.appcompat.app.ActionBar;
+import android.view.Menu;
+import android.widget.LinearLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
